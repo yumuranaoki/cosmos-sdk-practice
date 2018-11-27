af68e5637a84570f857668c2ef2930c8d0e9693e
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2018-11-27 17:12:40 +0900

    cli for query

--- x/practice/client/cli/query.go
+++ x/practice/client/cli/query.go
@@ -1,1 +1,17 @@
 package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/spf13/cobra"
+)
+
+func GetCmdBalance(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "balance [address]",
+		Short: "query balance of address",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+}
